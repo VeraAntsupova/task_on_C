80b1d513b3d5fc1198f67287b2fac4c82aabe1e8
Vera <vera.antsupova@yandex.ru>
2022-12-07 17:45:59 +0300

    решение задачи4

---
+++ task4(Sem1)/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Веведите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Веведите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Веведите 3-ье число: ");
+int p = Convert.ToInt32(Console.ReadLine());
+if (n>m)
+{
+    if (n>p)
+    {
+        Console.WriteLine(n);
+    }
+    else
+    {
+        Console.WriteLine(p);
+    }
+}
+else if (m>p)
+{
+    Console.WriteLine(m);
+}
+else 
+{
+    Console.WriteLine(p);
+}
+    
