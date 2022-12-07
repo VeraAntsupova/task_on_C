aa157a65617c7c0982486d6c123024624b1e1e26
Vera <vera.antsupova@yandex.ru>
2022-12-07 17:00:07 +0300

    Done hometasks

---
+++ Seminar1/Program.cs
@@ -0,0 +1,7 @@
+﻿// нахождение последней цифры в числе = нахождение остатка на 10 
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(n % 10);
+
+
