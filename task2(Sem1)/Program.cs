8c7d6cbc7b705556b321d4483012c10dd96fd18b
Vera <vera.antsupova@yandex.ru>
2022-12-07 17:19:12 +0300

    Решение задачи2

---
+++ task2(Sem1)/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите 1-ое число: ");
+   int n = Convert.ToInt32(Console.ReadLine()); //ввод чисел
+Console.WriteLine("Введите 2-ое число: "); 
+   int m = Convert.ToInt32(Console.ReadLine()); //ввод чисел
+   if (n > m)
+   {
+       Console.Write(n);
+   }
+   else if(n == m){
+        Console.Write("числа равны");
+   }
+   else
+   {
+       Console.Write(m);
+   }
+    
