5a68e47d32bd1a49355c27cda131d9501b1109e3
Vera <vera.antsupova@yandex.ru>
2022-12-13 20:01:01 +0300

    Решение задачи 15.Определение выходного дня.

---
+++ task15(Sem2)/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+ Console.Write ("ВВедите число: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ while ( n<1 || n>7)
+ {
+       Console.Write("Вы ошиблись, введите число от 1 до 7: ");
+       n = Convert.ToInt32(Console.ReadLine());
+ }
+if (n == 1)
+Console.WriteLine("Понедельник - не выходной");
+else if (n == 2)
+Console.WriteLine("Вторник - не выходной");
+else if (n == 3)
+Console.WriteLine("Среда - не выходной");
+else if (n == 4)
+Console.WriteLine("Четверг - не выходной");
+else if (n == 5)
+Console.WriteLine("Пятница - не выходной");
+else if (n == 6)
+Console.WriteLine("Суббота - Ура, выходной");
+else if (n == 7)
+Console.WriteLine("Воскресенье - Ура, выходной");
