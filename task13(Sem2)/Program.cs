587eec0d3883fcae83d690c0af92cb614eecb345
Vera <vera.antsupova@yandex.ru>
2022-12-13 19:01:46 +0300

    Решение задачи 13. Нахождение третьей цифры

---
+++ task13(Sem2)/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("ВВедите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int n3 = n % 10;
+if(n>=100)
+Console.Write (n3);
+else
+Console.Write ("третьей цифры нет");
