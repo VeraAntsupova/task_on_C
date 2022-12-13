8422762f2e100708c9589f5dbb135eda57b13dc7
Vera <vera.antsupova@yandex.ru>
2022-12-13 18:48:43 +0300

    вывод второй цифры трёхзначного числа

---
+++ task10(Sem2)/Program.cs
@@ -0,0 +1,6 @@
+﻿// ПРинимает на вход случайное трёхзначное число и показывает вторую цифру
+Console.Clear();
+Console.Write("ВВедите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n2 = n % 100 / 10;
+Console.Write (n2);
