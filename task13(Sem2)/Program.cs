6cd42e60e4f3cfe6b1b0b882e9f96949acba7fab
Vera <vera.antsupova@yandex.ru>
2022-12-15 14:12:17 +0300

    Решение задачи13. Нахождение третьей цифры

--- task13(Sem2)/Program.cs
+++ task13(Sem2)/Program.cs
@@ -1,10 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Clear();
-Console.Write("ВВедите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("ВВедите число: ");
+ int n = Convert.ToInt32(Console.ReadLine());
 
-int n3 = n % 10;
-if(n>=100)
-Console.Write (n3);
-else
-Console.Write ("третьей цифры нет");
+int count = n.ToString().Length;
+
+ 
+ if(count >= 3 ){
+
+int c=1;
+for (int i=count; i>3; i--){
+    c = c *10;
+}
+ int n3 = (n / c) % 10;
+ Console.Write (n3);
+ }else
+ Console.Write ("третьей цифры нет");
