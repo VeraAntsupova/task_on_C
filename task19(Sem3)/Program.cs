6cd686adc179df7c98e4f515ea14c26079fe1353
Vera <vera.antsupova@yandex.ru>
2022-12-16 17:38:39 +0300

    Решение задачи19. Палиндром ли?

---
+++ task19(Sem3)/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+ Console.Write ("ВВедите число: ");
+ string n = Console.ReadLine();
+
+while(n.Length != 5) 
+{
+    Console.Write("Вы ошиблись, введите пятизначное число: ");
+    n = Console.ReadLine();
+}
+
+
+if (n[0] == n[4] && n[1] == n[3])
+{
+    Console.WriteLine($"{n} - Палиндром");
+}
+else
+{
+    Console.WriteLine($"{n} - НЕ палиндром");
+}
+
+
+
+    
