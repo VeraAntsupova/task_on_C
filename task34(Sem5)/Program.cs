e9e7cce6868f0795142778171636aa03ab5f85d5
Vera <vera.antsupova@yandex.ru>
2022-12-24 13:57:38 +0300

    Решение задачи 34.Нахождение чётных числет в массиве, состоящем из трёхзначных положительных числел.

---
+++ task34(Sem5)/Program.cs
@@ -0,0 +1,25 @@
+﻿// tsk34  Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+Console.WriteLine("ВВедите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: {string.Join(",", array)}");
+Console.WriteLine($"Результат: {ReleaseArray(array)}");
