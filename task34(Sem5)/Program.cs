20380ce5c2bc59162a62318ba068f4726a5f0078
Vera <vera.antsupova@yandex.ru>
2022-12-24 17:12:47 +0300

    Решение задачи 34. Количество чётных числе в массиве.

--- task34(Sem5)/Program.cs
+++ task34(Sem5)/Program.cs
@@ -1,10 +1,9 @@
-﻿// tsk34  Задайте одномерный массив, заполненный случайными числами. 
-// Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// tsk34  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 void InputArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(-100, 100);
+        array[i] = new Random().Next(100, 1000);
 }
 
 int ReleaseArray(int[] array)
@@ -12,12 +11,12 @@ int ReleaseArray(int[] array)
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 == 1)
+        if (array[i] % 2 == 0)
             count++;
     }
     return count;
 }
-Console.WriteLine("ВВедите кол-во элементов массива: ");
+Console.WriteLine("ВВедите кол-во элементов: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 InputArray(array);
