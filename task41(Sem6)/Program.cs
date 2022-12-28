0a071c511f7b3e31e0bc3d7139d5d0171e171e89
Vera <vera.antsupova@yandex.ru>
2022-12-28 16:48:09 +0300

    Решение задачи 41. Количество чисел больше нуля.

---
+++ task41(Sem6)/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 100);
+}
+
+int ReleaseArray(int[] array)
+{
+    int count =0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >0 )
+        {
+            count++;
+        }
+        
+    }
+
+    return count;
+}
+Console.WriteLine("ВВедите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Массив: {string.Join(" | ", array)}");
+Console.WriteLine($"Результат: {ReleaseArray(array)}");
