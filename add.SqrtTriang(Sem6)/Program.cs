66f0312b06cb518112e7bcefce6679509a7cad45
Vera <vera.antsupova@yandex.ru>
2022-12-28 20:32:38 +0300

    Решение доп.задачи. Площадь треугольника.

---
+++ add.SqrtTriang(Sem6)/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите значения\nВершины А - x1 = ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("y1 = ");
+double y1 = double.Parse(Console.ReadLine());
+
+Console.Write("Вершины B - x2 = ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("y2 = ");
+double y2 = double.Parse(Console.ReadLine());
+
+Console.Write("Вершины C - x3 = ");
+double x3 = double.Parse(Console.ReadLine());
+Console.Write("y3 = ");
+double y3 = double.Parse(Console.ReadLine());
+
+double S;
+
+S = 0.5 * Math.Abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
+
+Console.WriteLine("Площадь треугольника равна: " + S);
