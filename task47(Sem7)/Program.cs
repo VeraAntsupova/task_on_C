1132b60041bcd97148d4a3ddbd98ad3cff0dd32d
Vera <vera.antsupova@yandex.ru>
2022-12-31 00:02:23 +0300

    Решение задачи 47. Двумерный массив с вещественными числами.

---
+++ task47(Sem7)/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте двумерный массив размером m * n, 
+// заполненный случайными вещественными числами
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 1; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    
+    }
+}
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+double[,] matrix = new double[Convert.ToInt32(numbers[0]), Convert.ToInt32(numbers[1])];
+InputMatrix(matrix);
