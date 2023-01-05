df899335b391de3d93e8cfa25161895b37d474b5
Vera <vera.antsupova@yandex.ru>
2023-01-05 09:31:19 +0300

    Решение задачи 60.Массив из неповторяющихся двузначных чисел.

---
+++ task60(Sem8)/Program.cs
@@ -0,0 +1,24 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
+
+void InPutMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(1, 11);
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k}) \t");
+            }
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите размеры матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,,] matrix = new int[coord[0], coord[1], coord[2]];
+InPutMatrix(matrix);
